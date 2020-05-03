fd28333c57be7e0f5e82b55a746e6488afbdd603
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2020-05-03 16:58:23 +0700

    init native base

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
-import * as React from "react";
+import React, { Component } from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "./src/screens/HomeScreen";
+import { View, Text } from "native-base";
 
 const Stack = createStackNavigator();
 
-export default function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName='Home'>
-				<Stack.Screen name='Home' component={HomeScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isReady: false,
+		};
+	}
+
+	async componentDidMount() {
+		await Font.loadAsync({
+			Roboto: require("native-base/Fonts/Roboto.ttf"),
+			Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+			...Ionicons.font,
+		});
+		this.setState({ isReady: true });
+	}
+
+	render() {
+		if (!this.state.isReady) {
+			return (
+				<View>
+					<Text>Loading...</Text>
+				</View>
+			);
+		}
+
+		return (
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName='Home'>
+					<Stack.Screen name='Home' component={HomeScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		);
+	}
 }
